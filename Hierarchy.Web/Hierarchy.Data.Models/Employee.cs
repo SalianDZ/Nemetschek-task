bd96622aa1abef7ee53766c4699d0b3f59c18f1e
Salian Dzhihanov <saliandzhihanov@gmail.com>
2024-11-03 17:32:36 +0200

    Created Employee class

---
+++ Hierarchy.Web/Hierarchy.Data.Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hierarchy.Data.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            Id = Guid.NewGuid();    
+            Subordinates = new HashSet<Employee>();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public int ExperienceYears { get; set; }
+
+        [ForeignKey("Position")]
+        public int PositionID { get; set; }
+
+        [ForeignKey("Department")]
+        public int DepartmentID { get; set; }
+
+        [ForeignKey("Supervisor")]
+        public int? SupervisorID { get; set; }
+
+        public Position Position { get; set; }
+        public Department Department { get; set; }
+        public Employee Supervisor { get; set; }
+        public ICollection<Employee> Subordinates { get; set; }
+
+    }
+}
