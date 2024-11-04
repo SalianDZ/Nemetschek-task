390bc8ef9c2111513d3bb41dd9bfbd74bdc2ebe1
Salian Dzhihanov <saliandzhihanov@gmail.com>
2024-11-04 20:04:41 +0200

    Added EmployeeProject Repository for the mapping table

---
+++ Hierarchy.Web/Hierarchy.Data.Repositories/EmployeeProjectRepository.cs
@@ -0,0 +1,30 @@
+﻿using Hierarchy.Data.Models;
+using Hierarchy.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hierarchy.Data.Repositories
+{
+    public class EmployeeProjectRepository : IEmployeeProjectRepository
+    {
+        private readonly HierarchyDbContext context;
+
+        public EmployeeProjectRepository(HierarchyDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<EmployeeProject>> GetEmployeeProjectsByEmployeeIdAsync(Guid employeeId)
+        {
+            return await context.Set<EmployeeProject>()
+                .Where(ep => ep.EmployeeID == employeeId)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<EmployeeProject>> GetEmployeeProjectsByProjectIdAsync(Guid projectId)
+        {
+            return await context.Set<EmployeeProject>()
+                .Where(ep => ep.ProjectID == projectId)
+                .ToListAsync();
+        }
+    }
+}
---
+++ Hierarchy.Web/Hierarchy.Data.Repositories/Interfaces/IEmployeeProjectRepository.cs
@@ -0,0 +1,12 @@
+﻿using Hierarchy.Data.Models;
+
+namespace Hierarchy.Data.Repositories.Interfaces
+{
+    public interface IEmployeeProjectRepository
+    {
+        //Task AssignEmployeeToProjectAsync(EmployeeProject employeeProject);
+        //Task RemoveEmployeeFromProjectAsync(Guid employeeId, Guid projectId);
+        Task<IEnumerable<EmployeeProject>> GetEmployeeProjectsByEmployeeIdAsync(Guid employeeId);
+        Task<IEnumerable<EmployeeProject>> GetEmployeeProjectsByProjectIdAsync(Guid projectId);
+    }
+}
