7994c1472f6a3de80f9a42f917f47258e16690ef
Salian Dzhihanov <saliandzhihanov@gmail.com>
2024-11-05 21:06:15 +0200

    Added method in the controller for all departments.

---
+++ Hierarchy.Web/Hierarchy.Web/Controllers/DepartmentController.cs
@@ -0,0 +1,23 @@
+﻿using Hierarchy.Services.Data;
+using Hierarchy.Services.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hierarchy.Web.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentService departmentService;
+
+        public DepartmentController(IDepartmentService departmentService)
+        {
+            this.departmentService = departmentService;
+        }
+
+
+        public async Task<IActionResult> All()
+        {
+            var departments = await departmentService.GetAllDepartmentsAsync();
+            return View(departments);
+        }
+    }
+}
